fe4b1dd601eddfd75628f3e53d11808aac48b0b9
gary boland <garyboland@gmail.com>
2016-02-22 00:04:12 +0000

    **argv == *argv[]

--- charstars.c
+++ charstars.c
@@ -4,6 +4,4 @@ int main(int argc, char * argv[]){
   printf("47 is slash\n");
   printf("** argv is %c\n", **argv);
   printf("filename is: %s\n", *argv);
-/*  printf("filename begins: %s\n", argv[0][0]);
- */
 }
