648d572b99de98e12b093654f7b0069ab05865b8
gary boland <garyboland@gmail.com>
2017-10-22 00:51:32 +0100

    return task

--- unsw_pointers.c
+++ unsw_pointers.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 void task (int x) {
-  printf("task x: %d\n", x);
+  printf("inside task x: %d\n", x);
+}
+int retask (int x) {
+  return x;
 }
 int main (int argc, char argv){
   int x = 2;
@@ -9,5 +12,6 @@ int main (int argc, char argv){
   printf("value at x: %d\n", x);
   printf("value at *p: %p\n", p);
   task(*p);
+  printf("return task: %d\n", retask(x));
 
 }
