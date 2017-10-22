6249024340e83f66d9c05e834469f3c92b0791a8
gary boland <garyboland@gmail.com>
2017-10-22 01:30:15 +0100

    increment with ref

--- unsw_pointers.c
+++ unsw_pointers.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-void task (int x) {
-  printf("inside task x: %d\n", x);
+void inctask (int *a) {
+  printf("value at a: %d\n", *a);
+  (*a)++;
 }
-int retask (int x) {
-  return x;
-}
-int main (int argc, char argv){
+int main (int argc, char *argv[]){
   int x = 2;
   int *p = &x;
-/*  int *p */
   printf("value at x: %d\n", x);
   printf("value at *p: %p\n", p);
-  task(*p);
-  printf("return task: %d\n", retask(x));
-
+  inctask(p);
+  printf("after inc: %d\n", x);
 }
