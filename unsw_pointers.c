63bf8058d35f3860acea13c9980d89e05e15a027
gary boland <garyboland@gmail.com>
2017-10-23 22:56:53 +0100

    points

--- unsw_pointers.c
+++ unsw_pointers.c
@@ -5,9 +5,12 @@ void inctask (int *a) {
 }
 int main (int argc, char *argv[]){
   int x = 2;
+  int *n;
   int *p = &x;
+  n = &x;
   printf("value at x: %d\n", x);
   printf("value at *p: %p\n", p);
   inctask(p);
   printf("after inc: %d\n", x);
+  printf("n is: %p\n", n);
 }
